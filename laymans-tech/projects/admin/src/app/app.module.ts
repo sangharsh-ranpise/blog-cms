41e949179c068cac6bf536674099ca424a2c31ef
sangharsh.ranpise <sangharsh002@gmail.com>
2019-03-10 12:08:44 +0530

    added admin nav bar

--- laymans-tech/projects/admin/src/app/app.module.ts
+++ laymans-tech/projects/admin/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminNavComponent } from './admin-nav/admin-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminNavComponent
   ],
   imports: [
     BrowserModule,
