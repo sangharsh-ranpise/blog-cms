fa6b553f88313dda37da4eb0e5bd01e442a119e8
sangharsh.ranpise <sangharsh002@gmail.com>
2019-03-10 13:30:12 +0530

    created component for dashboard

--- laymans-tech/projects/admin/src/app/app-routing.module.ts
+++ laymans-tech/projects/admin/src/app/app-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AdminBodyComponent } from './admin-body/admin-body.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const appRoutes: Routes = [
   {
     path: '',
     children: [
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
       {
         path: 'list/blogs',
         component: BlogListComponent
--- laymans-tech/projects/admin/src/app/app.module.ts
+++ laymans-tech/projects/admin/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AdminNavComponent } from './admin-nav/admin-nav.component';
 import { AdminBodyComponent } from './admin-body/admin-body.component';
 import { CommonModule } from '@angular/common';
 import { BlogListComponent } from './blog-list/blog-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminNavComponent,
     AdminBodyComponent,
-    BlogListComponent
+    BlogListComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
