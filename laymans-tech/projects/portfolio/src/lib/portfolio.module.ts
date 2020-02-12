fcf36e925d713fd9a0ea86f0412818d7e48a71ac
Sangharsh <sangharsh.ranpise@coditas.com>
2020-02-12 15:13:06 +0530

    Added about-me component

--- laymans-tech/projects/portfolio/src/lib/portfolio.module.ts
+++ laymans-tech/projects/portfolio/src/lib/portfolio.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PortfolioComponent } from './portfolio.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 
 @NgModule({
-  declarations: [PortfolioComponent],
+  declarations: [PortfolioComponent, AboutMeComponent],
   imports: [
   ],
   exports: [PortfolioComponent]
